b6650bdeb045e2104aa30e65f3a6bb49ca797c69
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2023-12-24 01:15:16 +0200

    Add init script for new devices

---
+++ dot_config/executable_init.sh
@@ -0,0 +1,27 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install chezmoi which manages the dotfiles
+brew install chezmoi
+
+# Use chezmoi to copy dotfiles
+chezmoi init git@github.com:JPaju/dotfiles.git
+chezmoi apply -v
+
+# Install brew packages
+brew bundle install --file ~/.config/brew/Brewfile
+
+# Install fisher packages
+fish -c "fisher update"
+
+# Setup JDK
+sdk install java 17.0.8-tem
+sdk use java 17.0.8-tem
+
+# Install Coursier packages
+cs install sbt
+cs install scala
+cs install scala-cli
+cs install metals
+cs install scalafmt
+cs install bloop
