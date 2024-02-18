89b9a9dbe41d1b103864d518d52b696b97f9d211
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-02-18 16:50:28 +0200

    Add betterdisplay, Logi options+ and smartthings(cli)

--- dot_config/nix-darwin/system/homebrew.nix
+++ dot_config/nix-darwin/system/homebrew.nix
@@ -6,16 +6,16 @@
     # onActivation.cleanup = "zap"; # TODO Uncomment to remove homebrew packages not listed below
     onActivation.autoUpdate = true;
 
-    taps = [ "dashlane/tap" "pbkit/homebrew-tap" ];
-    brews = [ "dashlane-cli" "clang-format" "pbkit" ];
+    taps = [ "dashlane/tap" "pbkit/homebrew-tap" "smartthingscommunity/smartthings" ];
+    brews = [ "dashlane-cli" "clang-format" "smartthings" "pbkit" ];
 
     caskArgs.no_quarantine = true;
     casks = let
       devTools = [ "dash" "docker" "jetbrains-toolbox" "postman" "visual-studio-code" ];
       terminal = [ "iterm2" ];
-      windowManagement = [ "alt-tab" "monitorcontrol" "rectangle" ];
+      windowManagement = [ "alt-tab" "betterdisplay" "monitorcontrol" "rectangle" ];
       productivity = [ "arc" "firefox" "google-chrome" "karabiner-elements" "notion" "numi" "signal" "spotify" ];
-      misc = [ "aldente" "appcleaner" "bartender" "menubarx" "stats" ];
+      misc = [ "aldente" "appcleaner" "bartender" "logi-options-plus" "menubarx" "stats" ];
     in devTools ++ terminal ++ windowManagement ++ productivity ++ misc;
   };
 
