965d166be93d4a7a28621b08acd39fe88892a4ab
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-04-03 13:59:55 +0300

    zellij: add zellij terminal multiplexer and default configuration

--- home/common.nix
+++ home/common.nix
@@ -14,6 +14,7 @@
     ./modules/secrets
     ./modules/starship
     ./modules/wezterm
+    ./modules/zellij
   ];
 }
 
---
+++ home/modules/zellij/default.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  programs = {
+    zellij = { enable = true; };
+
+    fish.shellAbbrs = {
+      za = "zellij attach";
+      zac = "zellij attach (basename (pwd)) --create";
+      zls = "zellij list-sessions";
+    };
+  };
+
+  xdg.configFile = { "zellij/config.kdl".source = ./config.kdl; };
+}
+
