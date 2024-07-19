2f12fa611e5fff7cfddea9eb24469cab1d683f91
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-07-19 10:22:08 +0300

    yazi: update theme and keybinds
    - Add file icons to catppuccin macchiato theme
    - Close input modals with just a single esc key press
    - Add smart-paste plugin
    - Show file owners in status bar

--- home/modules/cli-tools/yazi/default.nix
+++ home/modules/cli-tools/yazi/default.nix
@@ -3,7 +3,33 @@
     yazi = {
       enable = true;
       enableFishIntegration = true;
-      settings = { manager.ratio = [ 1 2 3 ]; };
+
+      settings = {
+        manager = {
+          ratio = [ 1 2 3 ];
+          show_hidden = true;
+          show_symlink = true;
+        };
+      };
+
+      initLua = ./init.lua;
+
+      keymap = {
+        manager.prepend_keymap = [{
+          run = "plugin --sync smart-paste";
+          on = [ "p" ];
+          desc = "Paste into the hovered directory or CWD";
+        }];
+
+        input.prepend_keymap = [{
+          run = "close";
+          on = [ "<Esc>" ];
+          desc = "Cancel input";
+        }];
+      };
+
+      plugins = { smart-paste = ./plugins/smart-paste.yazi; };
+
     };
   };
 
