faebd3488be5ddcf4b36d516ea53bb02c39130f5
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-08-11 17:39:44 +0300

    bat: change theme to catppuccin-macchiato

--- home/modules/cli-tools/bat/default.nix
+++ home/modules/cli-tools/bat/default.nix
@@ -1,10 +1,23 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs = {
+    fish.shellAliases = { cat = "bat"; };
+
     bat = {
       enable = true;
-      config = { theme = "TwoDark"; };
+      config = { theme = "catppuccin-macchiato"; };
+
+      themes = {
+        catppuccin-macchiato = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
+            sha256 = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
+          };
+          file = "themes/Catppuccin Macchiato.tmTheme";
+        };
+      };
     };
 
-    fish.shellAliases = { cat = "bat"; };
   };
 }
