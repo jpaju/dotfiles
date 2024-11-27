cee4c6ce5691657256590080eb1c435000cc0c6c
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-11-27 17:46:55 +0200

    starship: disable transient async prompt

--- home/modules/starship/default.nix
+++ home/modules/starship/default.nix
@@ -5,8 +5,5 @@
     package = pkgs-master.starship;
   };
 
-  xdg.configFile = {
-    "starship.toml".source = ./starship.toml;
-    "fish/conf.d/starship_async_transient_prompt.fish".source = ./starship_async_transient_prompt.fish;
-  };
+  xdg.configFile = { "starship.toml".source = ./starship.toml; };
 }
