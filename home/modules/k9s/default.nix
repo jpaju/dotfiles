02fe49daa657586193877bd92dcc0d11d9161428
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-11-28 14:21:06 +0200

    k9s: add configuration and use catppuccin theme

--- home/modules/k9s/default.nix
+++ home/modules/k9s/default.nix
@@ -1,1 +1,35 @@
-{ ... }: { programs.k9s = { enable = true; }; }
+{ pkgs, ... }: {
+  programs.k9s = {
+    enable = true;
+
+    # See: https://k9scli.io/topics/config/
+    settings.k9s = {
+      liveViewAutoRefresh = false;
+      refreshRate = 2;
+
+      ui = {
+        enableMouse = true;
+        headless = false;
+        reactive = false;
+        noIcons = false;
+      };
+
+      logger = {
+        tail = 100;
+        buffer = 2000;
+        sinceSeconds = -1;
+      };
+    };
+
+    skins = {
+      catppuccin-macchiato = let
+        theme = "catppuccin-macchiato";
+        rev = "4432383da214face855a873d61d2aa914084ffa2";
+      in pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/k9s/${rev}/dist/${theme}.yaml";
+        hash = "sha256-OR23zFdI/aQBL4cdqN/cnawEASRGw0voBP93QLzivfE=";
+      };
+    };
+
+  };
+}
