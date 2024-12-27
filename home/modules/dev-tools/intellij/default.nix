462233537a33e4027ca5a8fa181435e2b1e73286
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-12-27 15:17:07 +0200

    intellij: configure helix keybinds for IDEAVim

--- home/modules/dev-tools/intellij/default.nix
+++ home/modules/dev-tools/intellij/default.nix
@@ -1,1 +1,9 @@
-{ ... }: { home.file.".ideavimrc".source = ./.ideavimrc; }
+{ pkgs, ... }: {
+  home.file.".ideavimrc".source = let
+    rev = "cd495e540dd2736d5e39a686d787048070752fd4";
+    file = "helix.idea.vim";
+  in pkgs.fetchurl {
+    hash = "sha256-ANAPvdvQMacUPCPNsvwCtCD/dWhVW+IWa010WjoZZ40=";
+    url = "https://raw.githubusercontent.com/chtenb/helix.vim/${rev}/${file}";
+  };
+}
