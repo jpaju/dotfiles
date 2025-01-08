7a3973669ea5a5ab49b7e485b9663228ecaa624e
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-01-08 18:46:19 +0200

    gh: modify PR checkout script

--- home/modules/dev-tools/gh/gh_pr_switch.fish
+++ home/modules/dev-tools/gh/gh_pr_switch.fish
@@ -1,4 +1,6 @@
 function gh_pr_switch
+    argparse w/wait-on-error -- $argv
+
     set -x GH_FORCE_TTY 100% # Forces colored output for 'gh' command even when piped
 
     set -f selected_pr (
@@ -9,8 +11,27 @@ function gh_pr_switch
     )
 
     if test -z "$selected_pr"
+        echo "No PR selected, aborting."
         return 1
     end
 
-    gh pr checkout $selected_pr
+    # When not running in wait-on-error mode, don't wrap checkout command
+    if not set --query _flag_w
+        gh pr checkout $selected_pr
+    else
+        set output (gh pr checkout $selected_pr 2>&1)
+        set checkout_status $status
+
+        if test $checkout_status -ne 0
+            echo "Checkout failed. Error:"
+            echo "----------------------------------------------------------------"
+            echo -n (set_color red)
+            for line in $output; echo $line; end;
+            echo -n (set_color normal)
+            echo "----------------------------------------------------------------"
+
+            read --local --prompt-str "Press Enter to to continue..."
+            return $checkout_status
+        end
+    end
 end
