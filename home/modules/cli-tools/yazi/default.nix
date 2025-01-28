c0ede595c72d9144c4f36f7579840c0302c93094
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-01-28 13:18:13 +0200

    yazi: remove smart-paste plugin

--- home/modules/cli-tools/yazi/default.nix
+++ home/modules/cli-tools/yazi/default.nix
@@ -15,20 +15,12 @@
       initLua = ./init.lua;
 
       keymap = {
-        manager.prepend_keymap = [{
-          run = "plugin --sync smart-paste";
-          on = [ "p" ];
-          desc = "Paste into the hovered directory or CWD";
-        }];
-
         input.prepend_keymap = [{
           run = "close";
           on = [ "<Esc>" ];
           desc = "Cancel input";
         }];
       };
-
-      plugins = { smart-paste = ./plugins/smart-paste.yazi; };
 
     };
   };
