d494a39cf6b0808730a485ce0a088ee965883961
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-02-08 00:08:41 +0200

    gh: add markdown-preview extension

--- home/modules/dev-tools/gh/default.nix
+++ home/modules/dev-tools/gh/default.nix
@@ -1,17 +1,19 @@
 { pkgs, ... }: {
+  programs = {
 
-  home.packages = [
-    pkgs.gh
-    pkgs.fzf # Required by gh_pr_switch function
-  ];
+    gh = {
+      enable = true;
+      extensions = [ pkgs.gh-markdown-preview ];
+    };
 
-  programs.fish.shellAbbrs = {
-    ghb = "gh browse";
-    ghprc = "gh pr create --web";
-    ghprv = "gh pr view --web";
-    ghprm = "gh pr merge --squash --delete-branch";
-    ghrw = "gh run watch";
-    ghprs = "gh_pr_switch";
+    fish.shellAbbrs = {
+      ghb = "gh browse";
+      ghprc = "gh pr create --web";
+      ghprv = "gh pr view --web";
+      ghprm = "gh pr merge --squash --delete-branch";
+      ghrw = "gh run watch";
+      ghprs = "gh_pr_switch";
+    };
   };
 
   xdg.configFile."fish/functions/gh_pr_switch.fish".source = ./gh_pr_switch.fish;
