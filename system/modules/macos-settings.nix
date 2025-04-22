e9ae00bfbda2eb4c26182decb0a92df802f58b55
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-04-22 13:48:16 +0300

    macos: restructure settings

--- system/modules/macos-settings.nix
+++ system/modules/macos-settings.nix
@@ -5,13 +5,16 @@
   };
 
   system.defaults = {
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+
+    # Desktop & display
     spaces.spans-displays = false; # Displays have separate Spaces
+    NSGlobalDomain.AppleSpacesSwitchOnActivate = true; # Switch to space with open windows
+    dock.mru-spaces = false; # Auto-arrange spaces based on most recent use
+    dock.appswitcher-all-displays = true;
+    WindowManager.GloballyEnabled = false; # Disable Stage Manager
 
     dock = {
-      mru-spaces = false; # Auto-arrange spaces based on most recent use
-
-      appswitcher-all-displays = true;
-
       show-process-indicators = true;
       show-recents = false;
       autohide = true;
@@ -19,8 +22,6 @@
       largesize = 70;
       tilesize = 50;
     };
-
-    WindowManager.GloballyEnabled = false; # Disable Stage Manager
 
     # Menubar clock format: e.g 'd EEE h.mm.ss'
     menuExtraClock = {
@@ -41,20 +42,22 @@
       ShowPathbar = true;
     };
 
+    # Keyboard
     NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-
       ApplePressAndHoldEnabled = false; # Holding a key repeats it rather than showing the accent menu
       InitialKeyRepeat = 30;
       KeyRepeat = 2;
-
-      "com.apple.trackpad.scaling" = 1.0; # Trackpad tracking speed (0.0 - 3.0)
-      "com.apple.swipescrolldirection" = true; # Natural scrolling
       "com.apple.keyboard.fnState" = true; # Use function keys as standard function keys
     };
 
-    ".GlobalPreferences"."com.apple.mouse.scaling" = 0.875; # External mouse tracking speed (0.0 - 3.0)
+    # External mouse
+    ".GlobalPreferences"."com.apple.mouse.scaling" = 0.875; # Tracking speed (0.0 - 3.0)
 
+    # Trackpad
+    NSGlobalDomain = {
+      "com.apple.trackpad.scaling" = 1.0; # Tracking speed (0.0 - 3.0)
+      "com.apple.swipescrolldirection" = true; # Natural scrolling
+    };
     trackpad = {
       Clicking = false; # Disable tap to click
       ActuationStrength = 1; # Disable silent clicking
