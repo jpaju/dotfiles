6ffe2e5b5258198ca6f81d2c5261693dd1e094f9
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-05-26 22:34:59 +0300

    fish: remove unused nix abbreviations

--- home/modules/nix/default.nix
+++ home/modules/nix/default.nix
@@ -4,17 +4,8 @@
 
   programs.fish.shellAbbrs = {
     nxsh = "nix shell nixpkgs#";
-
     nxfu = "nix flake update";
-    ndu = "nix flake update --flake ~/dotfiles";
-
     nxd = "nix develop";
-    nxdbn = "nix develop path:$HOME/flakes/bun";
-    nxdsc = "nix develop github:devinsideyou/scala-seed#java17";
-    nxdts = "nix develop path:$HOME/flakes/typescript";
-    nxdpy = "nix develop path:$HOME/flakes/python";
-    nxdrs = "nix develop path:$HOME/flakes/rust";
-
     nds = "nix_darwin_switch";
   };
 
