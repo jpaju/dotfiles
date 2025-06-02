153c8e18ecc6afe8b5374095a9b6ec1cd903a85b
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-06-02 21:43:09 +0200

    bat: reformat nix module and configure man pager

--- home/modules/cli-tools/bat.nix
+++ home/modules/cli-tools/bat.nix
@@ -1,23 +1,22 @@
 { pkgs, ... }: {
-  programs = {
-    fish.shellAliases = { cat = "bat"; };
+  programs.fish = {
+    shellAliases.cat = "bat";
+    interactiveShellInit = "batman --export-env | source";
+  };
 
-    bat = {
-      enable = true;
-      config = { theme = "catppuccin-macchiato"; };
+  programs.bat = {
+    enable = true;
+    extraPackages = [ pkgs.bat-extras.batman ];
 
-      themes = {
-        catppuccin-macchiato = {
-          src = pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "bat";
-            rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
-            sha256 = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
-          };
-          file = "themes/Catppuccin Macchiato.tmTheme";
-        };
+    config.theme = "catppuccin-macchiato";
+    themes.catppuccin-macchiato = { # TODO Remove this once https://github.com/sharkdp/bat/pull/3317 is merged
+      src = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "bat";
+        rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
+        sha256 = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
       };
+      file = "themes/Catppuccin Macchiato.tmTheme";
     };
-
   };
 }
