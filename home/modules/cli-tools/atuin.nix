25a87ae6d6ab37f0fbb99b398a86f0ef504e233c
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-06-16 23:49:29 +0300

    atuin: refactor module

--- home/modules/cli-tools/atuin.nix
+++ home/modules/cli-tools/atuin.nix
@@ -1,15 +1,11 @@
 { ... }: {
-  programs = {
-    atuin = {
-      enable = true;
-      flags = [ "--disable-up-arrow" ];
-    };
-
-    fish = {
-      # Bind Ctrl+R to open atuin
-      interactiveShellInit = ''
-        bind \cr _atuin_search
-      '';
-    };
+  programs.atuin = {
+    enable = true;
+    flags = [ "--disable-up-arrow" ];
   };
+
+  # Bind Ctrl+R to open atuin
+  programs.fish.interactiveShellInit = ''
+    bind \cr _atuin_search
+  '';
 }
