b53c538ea4167dcb8d894721709807e31d669b77
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-06-17 00:03:35 +0300

    karabiner: remove automatic_backups/ and assets/ folders
    These should not be version controlled

--- home/modules/karabiner/default.nix
+++ home/modules/karabiner/default.nix
@@ -1,7 +1,3 @@
 { ... }: {
-  xdg.configFile = {
-    "karabiner/karabiner.json".source = ./karabiner.json;
-    "karabiner/assets".source = ./assets;
-    "karabiner/automatic_backups".source = ./automatic_backups;
-  };
+    xdg.configFile."karabiner/karabiner.json".source = ./karabiner.json;
 }
