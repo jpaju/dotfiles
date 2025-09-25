5192ab7944dad6f9ecb6d6bd9a7a0f49b45f16c5
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-09-25 23:56:24 +0300

    opencode: adjust settings

--- home/modules/ai/opencode/default.nix
+++ home/modules/ai/opencode/default.nix
@@ -2,21 +2,30 @@
 {
   imports = [ ../../secrets/interface.nix ];
 
+  programs.fish.shellAbbrs.oc = "opencode";
+
   programs.opencode = {
     enable = true;
 
     package = pkgs.writeShellScriptBin "opencode" ''
+      export OPENCODE_DISABLE_LSP_DOWNLOAD=true
+
       export ANTHROPIC_API_KEY="$(cat ${config.secrets.anthropic_api_key})"
       export OPENAI_API_KEY="$(cat ${config.secrets.openai_api_key})"
+
       exec ${pkgs.opencode}/bin/opencode "$@"
     '';
 
     settings = {
-      autoshare = false;
-      autoupdate = true;
-      theme = "tokyonight";
+      autoupdate = false;
+      share = "disabled";
+      theme = "catppuccin";
+      permission = {
+        edit = "ask";
+        bash = "ask";
+        webfetch = "ask";
+      };
     };
   };
 
-  programs.fish.shellAbbrs.oc = "opencode";
 }
