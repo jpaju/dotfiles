fc08d02be57ee0e6415efe9e6478119d8df04fa9
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-12-23 12:51:10 +0200

    init: gh-dash github TUI

--- home/modules/gh/default.nix
+++ home/modules/gh/default.nix
@@ -9,7 +9,19 @@
     ghprm = "gh pr merge --squash --delete-branch";
     ghrw = "gh run watch";
     ghprs = "gh_pr_switch";
+
+    ghd = "gh dash";
   };
+
+  programs.gh-dash = {
+    enable = true;
+    settings = {
+      defaults.preview.open = true;
+      defaults.preview.width = 80;
+      pager.diff = "delta";
+    };
+  };
+  catppuccin.gh-dash.enable = true;
 
   xdg.configFile."fish/functions/gh_pr_switch.fish".source = ./gh_pr_switch.fish;
 }
