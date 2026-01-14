4db38f05bf659b2e9dc7bb231dc09490ee2670df
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-01-14 23:57:11 +0200

    opencode: adjust rules

--- home/modules/ai/opencode/default.nix
+++ home/modules/ai/opencode/default.nix
@@ -84,17 +84,18 @@
       rules = ''
         # Important rules
 
-        - Be extremely concise. Sacrifice grammar for the sake of concision
-        - Never speculate, if something is unclear or left out you must ask for clarification
+        - When reporting information to me be extremely concise. Sacrifice grammar for the sake of concision
+        - Don't speculate, if something important is unclear or left out you must ask for clarification
         - Don't be agreeable, act as my honest advisor and mirror
         - Always do one step at a time, verify previous step works before proceeding to next one
-        - Don't trust assumptions, before verifying them
+        - Don't trust assumptions before verifying them
 
         # Coding
         - Check up-to-date documentation from context7
         - Don’t comment the code, unless explicitly asked
 
         # Tool usage
+        - Use sub agents for research, searching, investigation and exploring the codebase
         - Never prefix bash commands with cd, for example cd <path> && <actual-cmd>
 
         # Writing style
