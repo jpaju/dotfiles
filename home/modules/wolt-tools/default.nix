05292e50191150ef257c8833ba33516cdb7ba24c
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-01-16 09:38:49 +0200

    init: jira CLI for work profile

--- home/modules/wolt-tools/default.nix
+++ home/modules/wolt-tools/default.nix
@@ -6,9 +6,18 @@
 }:
 {
   config = lib.mkIf config.dotfiles.wolt-tools.enable {
-    home.packages = with pkgs; [
-      gnupg
-      vault
-    ];
+    home.packages =
+      let
+        jiraWrapped = pkgs.writeShellScriptBin "jira" ''
+          export JIRA_API_TOKEN="$(op read "op://Private/7372wd5ekuqogdq3btpe7ncuyi/credential")"
+          exec ${pkgs.jira-cli-go}/bin/jira "$@"
+        '';
+      in
+      with pkgs;
+      [
+        gnupg
+        jiraWrapped
+        vault
+      ];
   };
 }
