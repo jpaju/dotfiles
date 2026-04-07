e1c1353757588b9a26c1eeba5a1f84d68626862c
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-04-07 16:24:59 +0300

    fish: adjust abbreviations
    - remove unused abbreviations

--- home/base/fish/conf.d/abbreviations.fish
+++ home/base/fish/conf.d/abbreviations.fish
@@ -13,12 +13,9 @@ abbr --add nti 'pnpm run test:integration'
 abbr --add nv  'pnpm --version'
 
 # Misc
-abbr --add cb               'fish_clipboard_copy'
-abbr --add cpuuid           'uuidgen | fish_clipboard_copy'
 abbr --add jps              'jps -lm'
 abbr --add loc              'tokei --num-format underscores'
 abbr --add lt               'ls --tree --level 3'
 abbr --add reload           'exec fish'
+abbr --add rl               'exec fish'
 abbr --add utcnow           'date -u +%Y-%m-%dT%H:%M:%SZ'
-abbr --add ya               'yazi'
-alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
