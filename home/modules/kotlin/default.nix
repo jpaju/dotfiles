d174e1e947c03b595a6c2cbba2aef804a23ab164
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-06-12 21:27:29 +0300

    kotlin: remove unofficial/unmaintained kotlin-language-server

--- home/modules/kotlin/default.nix
+++ home/modules/kotlin/default.nix
@@ -1,15 +1,10 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   config = lib.mkIf config.dotfiles.kotlin.enable {
-    home.packages = with pkgs; [
-      kotlin-language-server
-    ];
-
     home.file."flakes/kotlin/flake.nix".source = ./flake.nix;
   };
 }
